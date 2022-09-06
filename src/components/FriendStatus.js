1a3b9041027b20c1c2ff284497663384b04f3c54
Garrett Hays <GarrettlHays@gmail.com>
2022-09-06 14:40:27 -0700

    adds toast to fiend status component
    Co-authored-by: zbl14 <zbl14@users.noreply.github.com>

--- src/components/FriendStatus.js
+++ src/components/FriendStatus.js
@@ -1,19 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Toast from 'react-bootstrap/Toast';
 
-function FriendStatus(props){
+function FriendStatus(props) {
   return (
-    <React.Fragment>
-      <h3>{props.name}</h3>
-      <p>{props.status}</p>
-      <hr/>
-    </React.Fragment>
+    <Toast className="mb-4">
+      <Toast.Header>
+        <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+        <strong className="me-auto">{props.name}</strong>
+        <small>{props.time} mins ago</small>
+      </Toast.Header>
+      <Toast.Body>{props.status}</Toast.Body>
+    </Toast>
   );
 }
 
 FriendStatus.propTypes = {
   name: PropTypes.string.isRequired,
   status: PropTypes.string.isRequired,
+  time: PropTypes.number.isRequired,
 };
 
 export default FriendStatus;
+
+
+
