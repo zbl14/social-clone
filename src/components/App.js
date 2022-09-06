11531ba883af0c346fbbd7a09bc1b31575ae3566
Garrett Hays <GarrettlHays@gmail.com>
2022-09-06 14:40:09 -0700

    makes updates to app js file
    Co-authored-by: zbl14 <zbl14@users.noreply.github.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
 import React from "react";
-import Header from "./Header";
 import NavBar from "./NavBar";
 import User from "./User";
 import PostList from "./PostList";
 import AddFriend from "./AddFriend";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App(){
-  return ( 
+function App() {
+  return (
     <React.Fragment>
-      <Header />
-      <NavBar />
-      <User />
-      <PostList />
-      <AddFriend />
+      <Container>
+        <NavBar />
+        <Row>
+          <Col><User /></Col>
+          <Col><PostList /></Col>
+          <Col><AddFriend /></Col>
+        </Row>
+      </Container>
     </React.Fragment>
   );
 }
