1580973bf7be9ead336bd5db01e7c99b3b805d9d
Garrett Hays <GarrettlHays@gmail.com>
2022-09-06 14:40:54 -0700

    updates new friend js to include cards
    Co-authored-by: zbl14 <zbl14@users.noreply.github.com>

--- src/components/NewFriend.js
+++ src/components/NewFriend.js
@@ -1,19 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function NewFriend(props){
+function NewFriend(props) {
   return (
-    <React.Fragment>
-      <img src="" alt="friend"/>    
-      <h3>{props.name}</h3>
-      <button href="#">ADD FRIEND</button>
-      <hr/>
-    </React.Fragment>
+      <Card className="mt-2 shadow-lg p-1 mb-3 bg-white rounded" style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={props.img}/>
+        <Card.Body className="">
+          <Row>
+            <Col><Card.Title>{props.name}</Card.Title></Col>
+            <Col><Button className="btn btn-outline-success" variant="light" size="sm">Add Friend</Button></Col>
+          </Row>
+        </Card.Body>
+      </Card>
   );
 }
 
 NewFriend.propTypes = {
   name: PropTypes.string.isRequired,
+  img: PropTypes.string
 };
 
 export default NewFriend;
