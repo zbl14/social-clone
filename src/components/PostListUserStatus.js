4a47793e45b7bb1ceb1fdc20e72dbd106657748f
Garrett Hays <GarrettlHays@gmail.com>
2022-09-06 14:41:48 -0700

    makes adjustments to post list columns
    Co-authored-by: zbl14 <zbl14@users.noreply.github.com>

--- src/components/PostListUserStatus.js
+++ src/components/PostListUserStatus.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const PostListUserStatus = () => {
   return(
-    <form>
-      <input type="text" placeholder="What's Happening?" />
-      <button href="#">Submit</button>
-    </form>
+    <Container fluid>
+      <form>
+        <Row>
+          <Col md={7}><input type="text" placeholder="What's Happening?" /></Col> 
+          <Col md={2}><Button className="btn btn-outline-success" variant="light" size="sm" href="#">Submit</Button></Col>
+        </Row>
+      </form>
+    </Container>
   );
 };
 
