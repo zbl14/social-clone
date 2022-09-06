6bb3bd7ff54cdf8ce10f74dcfed071d2d6f1388b
Garrett Hays <GarrettlHays@gmail.com>
2022-09-06 14:39:55 -0700

    updates add friend js file
    Co-authored-by: zbl14 <zbl14@users.noreply.github.com>

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -1,13 +1,18 @@
 import React from "react";
 import AddFriendFriendInfo from "./AddFriendFriendInfo";
+import Container from 'react-bootstrap/Container';
 
 const AddFriend = () => {
   return(
     <React.Fragment>
-      <p>Some people you may know:</p>
-      <AddFriendFriendInfo />
+      <Container className="mt-5">
+        <h5>Friend Suggestions:</h5>
+        <AddFriendFriendInfo />
+      </Container>
     </React.Fragment>
   )
 };
 
 export default AddFriend;
+
+
