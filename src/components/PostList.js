00cdb239cb64bf70d0edb49af877dd764624aca1
Garrett Hays <GarrettlHays@gmail.com>
2022-09-06 14:41:30 -0700

    updates postlist container

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,16 @@
 import React from "react";
 import PostListUserStatus from "./PostListUserStatus";
 import PostListFriendStatus from "./PostListFriendStatus";
+import Container from 'react-bootstrap/Container';
 
 
 const PostList = () => {
   return(
     <React.Fragment>
-      <PostListUserStatus />
-      <PostListFriendStatus />
+      <Container className="mt-5">
+        <PostListUserStatus />
+        <PostListFriendStatus />
+      </Container>
     </React.Fragment>
   )
 };
