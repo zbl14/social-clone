a4ae366687d238552c7de428b7749ca7fc56d194
Garrett Hays <GarrettlHays@gmail.com>
2022-09-06 14:42:21 -0700

    updates userinfo and buttons
    Co-authored-by: zbl14 <zbl14@users.noreply.github.com>

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,13 +1,19 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 const UserInfo = () => {
   return(
     <React.Fragment>
-      <img src="" alt="me"/>
-      <p>TestUser</p>
-      <button href="#">TWEETS</button>
-      <button href="#">FOLLOWING</button>
-      <button href="#">FOLLOWERS</button>
+      <br/>
+      <img src="https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=620&quality=45&fit=max&dpr=2&s=ab46a8887cfbbb1068020c58ce592306" alt="user" width = "200 px"/>
+      <h1>Tom KatChat</h1>
+      <br/>
+      <ButtonGroup>
+        <Button className="btn btn-outline-success" variant="light"  href="#">Tweets: 25</Button>
+        <Button className="btn btn-outline-success" variant="light" href="#">Following: 10</Button>
+        <Button className="btn btn-outline-success" variant="light" href="#">Followers: 2</Button>
+      </ButtonGroup>
     </React.Fragment>
   );
 };
